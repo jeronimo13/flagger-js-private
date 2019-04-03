df3ad3e770a5ed060b50d7997bccea0d9f12ace1
herman.havrysh <herman.havrysh@sandbx.co>
2019-04-03 23:24:38 +0300

    removed axios

--- v3/src/Flag.js
+++ v3/src/Flag.js
@@ -1,13 +1,10 @@
-// Builtins and externals
-import axios from 'axios'
-
 // Internals
 import FlagCore from './core/Flag'
 
 export default class Flag extends FlagCore {
   // Public API
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
   }
 }
